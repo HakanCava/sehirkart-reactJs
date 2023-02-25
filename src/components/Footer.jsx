e84d3e44d8e07849c07e6f3e20ade46340d48550
HakanCava <hakancavaa@gmail.com>
2023-02-25 10:23:47 +0300

    update1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,12 @@ import {FcPortraitMode} from "react-icons/fc"
 const Footer=()=>{
     return (
         <Row className="bg-dark p-3 align-items-center rounded-4 ">
-            <p className="text-info m-auto">Copyright by Hakan <FcPortraitMode/>  {new Date().getFullYear()}</p>
-        </Row>
+        <p className="text-info m-auto">Copyright by Hakan <FcPortraitMode/>  {new Date().getFullYear()}</p>
+        </Row> 
     )
 }
 
 export default Footer
+
+
+
